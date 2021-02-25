07e4c7a168b018a1538d555a2b9cb2cc0e303503
Arkh3 <ateruelfe@gmail.com>
2021-02-25 11:30:39 +0100

    pr1: enunciado + parte1 cambiada un poco

--- practica1/practica1_1/src/hilos.java
+++ practica1/practica1_1/src/hilos.java
@@ -1,42 +1,27 @@
-public class hilos extends Thread{
+
+
+public class Hilos extends Thread{
 	
-	private int id;
-	private static int N = 5;
-	private int T = 100;
+	int id;
+	int restTime;
 	
-	public hilos (int id) {
-		this.id = id;
+	Hilos (int identificador, int t){
+		
+		this.id = identificador;
+		this.restTime = t;
 	}
 	
 	public void run() {
+		
+		System.out.println("Soy el hilo:" + id);
+		
 		try {
-			sleep(T);
+			sleep(restTime);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Soy el hilo:"+id);
-	}
-	
-	public static void main(String[] args) {
 		
-		hilos[] vecHilos = new hilos[N];
-		
-		for (int i = 0; i< vecHilos.length; i++) {
-			vecHilos[i] = new hilos(i+1);
-			vecHilos[i].start();
-		}
-		
-		try {
-			for (int i = 0; i< vecHilos.length; i++) {
-				
-				vecHilos[i].join();
-				
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-
-		System.out.println("Se ha terminado");
+		System.out.println("Soy el hilo:" + id);
 	}
 }
